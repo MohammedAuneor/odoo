aab45e21b4498c3a3d90c3385253328703cf4d7f
Angelo-Auneor <angelo@auneor-conseil.fr>
2018-11-19 12:00:46 +0100

    no expiration database

--- addons/fleet/models/fleet.py
+++ addons/fleet/models/fleet.py
@@ -541,13 +541,7 @@ class FleetVehicleLogContract(models.Model):
         otherwise return the number of days before the contract expires
         """
         for record in self:
-            if (record.expiration_date and (record.state == 'open' or record.state == 'toclose')):
-                today = fields.Date.from_string(fields.Date.today())
-                renew_date = fields.Date.from_string(record.expiration_date)
-                diff_time = (renew_date - today).days
-                record.days_left = diff_time > 0 and diff_time or 0
-            else:
-                record.days_left = -1
+            record.days_left = 100
 
     @api.depends('cost_ids.amount')
     def _compute_sum_cost(self):
